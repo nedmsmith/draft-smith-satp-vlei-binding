c30357dd9056a98c084d4ef09452f3aee52d1a1a
Ned Smith <ned.smith.ietf@outlook.com>
2025-09-04 19:18:38 -0700

    Update tools.mk
    fix cddlc

--- cddl/tools.mk
+++ cddl/tools.mk
@@ -6,7 +6,7 @@ ifeq ($(strip $(cddl)),)
 $(error cddl not found. To install cddl: 'gem install cddl')
 endif
 
-cddlc ?= $(shell command -v cddl)
+cddlc ?= $(shell command -v cddlc)
 ifeq ($(strip $(cddlc)),)
 $(error cddlc not found. To install cddlc: 'gem install cddlc')
 endif
